19bb85ef3ee52bfa137dfd6fe8ef4cc8ada97f58
MoonfireSeco <haaloplaysminecraft@gmail.com>
2018-10-03 18:42:31 +0300

    Update LunaRun.sh

--- LunaBot Bundle/LunaRun.sh
+++ LunaBot Bundle/LunaRun.sh
@@ -1,3 +1,3 @@
 chromium --user-data-dir="/home/cadence/LunaBot" --disable-web-security &
-xterm -e python smtpserver &
+xterm -e python3 smtpserver &
 while true; do xterm -e python LunaBotDiscord.py; done
