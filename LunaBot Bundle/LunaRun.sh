7cc6dc19d7ec9a14a09aa89e9fd0f27c3d234349
MoonfireSeco <haaloplaysminecraft@gmail.com>
2018-10-24 21:46:45 +0300

    Add files via upload

--- LunaBot Bundle/LunaRun.sh
+++ LunaBot Bundle/LunaRun.sh
@@ -1,3 +1,3 @@
 chromium-browser --user-data-dir="/home/amy/Desktop/LunaBot/LunaBot" --disable-web-security web.whatsapp.com &
-xterm -e python3 /home/amy/Desktop/LunaBot/smtpserver &
+xterm -e python3 /home/amy/Desktop/LunaBot/LunaBotBackend &
 #while true; do xterm -e 'python3 /home/amy/Desktop/LunaBot/LunaBotDiscord.py'; done
